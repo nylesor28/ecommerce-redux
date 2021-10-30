86c16d2c68f2687d083bae733ebe5d5253b905fd
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-29 21:00:54 -0400

    production build

---
+++ client/build/precache-manifest.6caa049874291b4a54a1e90c4ab09033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9b3216de8fcc5b531f6305d1f9a1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72a2c0be4cc981a82a8b",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "3948ed69620c9eb1175d",
+    "url": "/static/js/2.f9acd560.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.f9acd560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a2c0be4cc981a82a8b",
+    "url": "/static/js/main.b74b0ee2.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
